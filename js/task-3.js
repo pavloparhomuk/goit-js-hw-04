b9c3b3b3be63515abe89c5242af29faca3131f91
Parhomuk_Pavlo <pavelparxomuk@gmail.com>
2024-04-28 21:47:53 +0300

    add new files

--- js/task-3.js
+++ js/task-3.js
@@ -3,13 +3,13 @@ const profile = {
   playTime: 300,
 
   changeUsername(newName) {
-    this.username === newName;
+    this.username = newName;
   },
   updatePlayTime(hours) {
     this.playTime += hours;
   },
   getInfo() {
-    return `${this.Username} has ${this.playTime} active hours!`;
+    return `${this.username} has ${this.playTime} active hours!`;
   },
 };
 
